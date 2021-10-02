550ad403446a0ee473b5c588db26dc747920657d
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-10-02 14:35:59 +0300

    feat: add actions in actions.js

--- src/modules/actions.js
+++ src/modules/actions.js
@@ -1,6 +1,22 @@
-// Project
-export const CREATE_PROJECT = 'CREATE_PROJECT';
-export const DELETE_PROJECT = 'DELETE_PROJECT';
-export const LOAD_PROJECTS = 'LOAD_PROJECTS';
-export const LOAD_PROJECTS_SUCCESS = 'LOAD_PROJECTS_SUCCESS';
-export const LOAD_PROJECTS_FAILURE = 'LOAD_PROJECTS_FAILURE';
+
+export const types = {
+    CREATE_PROJECT: 'CREATE_PROJECT',
+    DELETE_PROJECT: 'DELETE_PROJECT',
+    LOAD_PROJECTS: 'LOAD_PROJECTS',
+    LOAD_PROJECTS_SUCCESS: 'LOAD_PROJECTS_SUCCESS',
+    LOAD_PROJECTS_FAILURE: 'LOAD_PROJECTS_FAILURE'
+}
+
+export const loadProjects = () => ({type: types.LOAD_PROJECTS});
+
+export const loadProjectsSuccess = (projects) =>
+    ({
+        type: types.LOAD_PROJECTS_SUCCESS,
+        projects,
+    });
+
+export const loadProjectsFailure = (error) =>
+    ({
+        type: types.LOAD_PROJECTS_FAILURE,
+        error,
+    });

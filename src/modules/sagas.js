c5338fb383501b64d3cbc2ac75cea56ece1d9761
Artem Goncharov <32834710+artemgoncharoff@users.noreply.github.com>
2021-10-02 14:51:58 +0300

    feat: add projectsSaga

---
+++ src/modules/sagas.js
@@ -0,0 +1,20 @@
+import { takeEvery, put, call } from 'redux-saga/effects';
+import * as api from '../lib/api-client';
+import {loadProjectsFailure, loadProjectsSuccess, types as t} from "./actions";
+
+function* loadProjectsSaga() {
+    try {
+        const res = yield call(api.getProjects);
+        const projects = res.data;
+
+        yield put(loadProjectsSuccess(projects));
+    } catch (error) {
+        yield put(loadProjectsFailure(error.toString()));
+    }
+}
+
+function* projectsSaga() {
+    yield takeEvery(t.LOAD_PROJECTS_REQUEST, loadProjectsSaga);
+}
+
+export default projectsSaga;
